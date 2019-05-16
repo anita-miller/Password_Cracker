28f6cf86e652a685f1832557e161a663022ef90a
anita naseri <anita.naseri96@gmail.com>
2019-05-16 13:38:08 +1000

    adding function to read crack with 2 arguemnts + constants to address 6 word guesses

--- main.c
+++ main.c
@@ -21,11 +21,23 @@
 // Constants
 #define FIRST_GUESS_COMMON_PASSWORDS  "common_passwords.txt"
 #define PASSWORD_FILE  "pwd4sha256"
-
+/*
+static const char *DATABASE_COMMON_PASSWORDS = "6_letters/database_common_passwords.txt";
+static const char *DATABASE_NAMES_MALE_LOWER = "6_letters/database_sixLetter_names_male_lower.txt";
+static const char *DATABASE_NAMES_MALE_UPPER = "6_letters/database_sixLetter_names_male_upper.txt";
+static const char *DATABASE_NAMES_FEMALE_LOWER = "6_letters/database_sixLetter_names_female_lower.txt";
+static const char *DATABASE_NAMES_FEMALE_UPPER = "6_letters/database_sixLetter_names_female_upper";
+static const char *DATABASE_1000_NOUNS_LOWER = "6_letters/database_sixLetter_nouns_1000_lower.csv";
+static const char *DATABASE_1000_NOUNS_UPPER = "6_letters/database_sixLetter_nouns_1000_upper.csv";
+static const char *DATABASE_1000_VERBS_LOWER = "6_letters/database_sixLetter_verbs_1000_lower.csv";
+static const char *DATABASE_1000_VERBS_UPPER = "6_letters/database_sixLetter_verbs_1000_upper.csv";
+static const char *DATABASE_1000_ADJEC_LOWER = "6_letters/database_sixLetter_adjectives_1000_lower.csv";
+static const char *DATABASE_1000_ADJEC_UPPER = "6_letters/database_sixLetter_adjectives_1000_upper.csv";
+*/
 char **getwords(FILE *fp, int *n);
 void free_array(char **words, int rows);
 void create_hash(BYTE text[], BYTE hash[SHA256_BLOCK_SIZE]);
-unsigned char** readSHAfilePasswords();
+BYTE *readSHAfilePasswords();
 void crack_noargument();
 int checkPasswords(BYTE *passwords, char *guess); 
 
@@ -65,10 +77,11 @@ int main(int argc, char **argv)
 		}
 
 		// Part 3: three arguments to crack.exe
-		else if (argc == 3)
-		{
-			crack_oneargument(words[i], , argv[0], argv[1]);
-		}*/
+	*/	
+	else if (argc == 3)
+	{
+			crack_threeargument(argv[1], argv[2]);
+	}
 	else
 	{
 		perror("can not accept 3 inputs");
@@ -148,7 +161,7 @@ void create_hash(BYTE text[], BYTE hash[SHA256_BLOCK_SIZE])
 }
 
 //read file containing SHA Passwords and returns the bite array
-unsigned char** readSHAfilePasswords()
+BYTE *readSHAfilePasswords()
 {
 	FILE *dictionary;
 	// Store the binary file as an array of hex values
@@ -191,7 +204,16 @@ int checkPasswords(BYTE *passwords, char *guess)
 	return -1;
 }
 
+void crack_MostCommonWords(char* word) {
+	/*BYTE *passwords;
+	passwords = readSHAfilePasswords();
 
+	if (checkPasswords(passwords, word) != -1)
+	{
+		printf("%s\n", word);
+		printf("%d\n", checkPasswords(passwords, word));
+	}*/
+}
 
 void crack_noargument()
 {
@@ -205,14 +227,6 @@ void crack_noargument()
 	{
 		fread(passwords[i], sizeof(passwords[i]), 1, pwd4sha256);
 	}
-
-	// check the list of hash passwrod against word from list of most common words
-	//if (checkPasswords(passwords, word) != -1){
-	//	printf("%s\n", word);
-	//	printf("%d\n", checkPasswords(passwords, word));
-	//}
-
-	char* text = "should stop";
 
 	// 4-letters passwords: Brute Force
 	for (int i = 32; i <= 126 && flag == 1; i++)
@@ -263,14 +277,77 @@ void crack_noargument()
 	}
 }
 
-/*void crack_twoargument()
+//void crack_twoargument()
+//{
+
+//}
+
+void crack_threeargument(char *guesses_file, char *hashes_file)
 {
 
+	FILE *inputFile_guesses;
+	FILE *inputFile_hashes;
+	unsigned char hashes[MAX_WORD_LEN][SHA256_BLOCK_SIZE];
+	int count_input_guesses;
+	char curr_guess[MAX_WORD_LEN];
+	int count_matched_hash;
+
+	//opening both files
+	inputFile_guesses = fopen(guesses_file, "r");
+	inputFile_hashes = fopen(hashes_file, "rb");
+
+	/* validate file open */
+	if (!inputFile_hashes || !inputFile_guesses)
+	{
+		fprintf(stderr, "error: file open failed.\n");
+		return 1;
+	}
+	
+	count_input_guesses = 0;
+	while (fread(hashes[count_input_guesses], sizeof(hashes[count_input_guesses]), 1, inputFile_hashes) == 1)
+	{
+		count_input_guesses++;
+	}
+	printf("%02x, %d\n", hashes[0][0], count_input_guesses);
+
+	// for each word in the guesses file
+	while (fgets(curr_guess, MAX_WORD_LEN, inputFile_guesses))
+	{
+		int wordlen = strlen(curr_guess);
+
+		// strip '\n'
+		if (curr_guess[wordlen - 1] == '\n')
+			curr_guess[--wordlen] = 0;
+
+		// Hash the guessed password
+		BYTE result[SHA256_BLOCK_SIZE];
+		create_hash((BYTE *)curr_guess, result);
+
+		// Compare the hash to the array of hash 
+		for (int i = 0; i < count_input_guesses; i++)
+		{
+			count_matched_hash = 0;
+			// for every value from 0 to 32
+			for (int j = 0; j < SHA256_BLOCK_SIZE; j++)
+			{
+				if (hashes[i][j] == result[j])
+				{
+					count_matched_hash++;
+				}
+			}
+
+			// password found
+			if (count_matched_hash == SHA256_BLOCK_SIZE)
+			{
+				printf("%s %d\n", curr_guess, i + 1);
+				break;
+			}
+		}
+	}
+
+	//opening both files closing both files
+	fclose(inputFile_guesses);
+	fclose(inputFile_hashes);
 }
 
-void crack_twoargument()
-{
 
-}
-
-*/
